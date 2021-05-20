9e0189b197a4bd9c96f0784c99c24b334ee7653a
Oghenetega.peter <Oghenetega.peter@WEMA-ITS-L8755>
2021-05-20 11:16:45 +0100

    updated checkYuGiOh

--- checkYuGiOh.js
+++ checkYuGiOh.js
@@ -3,16 +3,14 @@ const checkYuGiOh = (n) => {
   console.log(arr);
 
   for (i in arr) {
-    arr[i] % 2 === 0 ? arr[i] = "Yu" : null
-    arr[i] % 3 === 0 ? arr[i] = "Gi" : null
-    arr[i] % 5 === 0 ? arr[i] = "Oh" : null
-
-    // arr[i] % 2 === 0  && arr[i].length > 1 ?  arr[i] = "Yu-Gi" : i
-    
+    arr[i] % 2 === 0 ? (arr[i] = "Yu") : i;
+    arr[i] % 3 === 0 ? (arr[i] = "Gi") : i;
+    arr[i] % 5 === 0 ? (arr[i] = "Oh") : i;
+    // console.log(i);
   }
 
   if (isNaN(n)) {
-    console.log(`invalid parameter: "${n}".`)
+    console.log(`invalid parameter: "${n}".`);
   }
 };
 checkYuGiOh("5");
